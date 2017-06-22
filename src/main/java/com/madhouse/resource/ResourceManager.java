20229336a0ee2a1dafc5bffe9d597ee43bda24c6
wujunfeng <wujunfeng@madhouse-inc.com>
2017-06-22 17:47:22 +0800

    update

--- src/main/java/com/madhouse/resource/ResourceManager.java
+++ src/main/java/com/madhouse/resource/ResourceManager.java
@@ -10,10 +10,16 @@ import com.madhouse.ssp.LoggerUtil;
 import com.madhouse.util.ObjectUtils;
 import com.madhouse.util.StringUtil;
 
+import org.apache.commons.lang3.tuple.Pair;
 import sun.security.jca.GetInstance;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
 
 /**
  * Created by WUJUNFENG on 2017/5/23.
@@ -23,8 +29,7 @@ public class ResourceManager {
     private ConcurrentHashMap<Integer, DSPBaseHandler> dspBaseHandlerMap = new ConcurrentHashMap<Integer, DSPBaseHandler>();
     private ConcurrentHashMap<Integer, MediaBaseHandler> mediaBaseHandlerMap = new ConcurrentHashMap<Integer, MediaBaseHandler>();
     private ConcurrentHashMap<String, Integer> mediaApiType = new ConcurrentHashMap<String, Integer>();
-    private ConcurrentLinkedDeque<pai> locations = new ConcurrentLinkedDeque<>();
-
+    private final ArrayList<Pair<Long, String>> iptables = this.loadLocations();
     private final Premiummad premiummad = JSON.parseObject(ObjectUtils.ReadFile(ResourceManager.class.getClassLoader().getResource("config.json").getPath()), Premiummad.class);
 
     private static final ResourceManager resourceManager = new ResourceManager();
@@ -33,11 +38,42 @@ public class ResourceManager {
         return resourceManager;
     }
 
+    public ArrayList loadLocations() {
+        try {
+            ArrayList<Pair<Long, String>> iptables = new ArrayList<>();
+            File file = new File(ResourceManager.class.getClassLoader().getResource("locations.dat").getPath());
+
+            if (file.isFile() && file.exists()) {
+                InputStreamReader reader = new InputStreamReader(new FileInputStream(file), "utf-8");
+                BufferedReader bufferedReader = new BufferedReader(reader);
+
+                String text = null;
+                while((text = bufferedReader.readLine()) != null) {
+                    String[] var1 = text.split(",");
+                    if (var1.length >= 3) {
+                        String[] var2 = var1[0].split(".");
+                        if (var2.length >= 4) {
+                            Long addr = (Long.parseLong(var2[0]) << 24) | (Long.parseLong(var2[1]) << 16 ) | (Long.parseLong(var2[2]) << 8) | Long.parseLong(var2[3]);
+                            iptables.add(Pair.of(addr, var1[2]));
+                        }
+                    }
+                }
+
+                reader.close();
+                return iptables.isEmpty() ? null : iptables;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
     public boolean init()
     {
         this.kafkaProducer = new KafkaProducer("", 1048576, 8, null);
-        
-        
+        this.loadLocations();
+
         for (Bid bid : premiummad.getWebapp().getBids())
         {
             this.mediaApiType.put(bid.getPath(),bid.getType());
@@ -63,7 +99,29 @@ public class ResourceManager {
     }
 
     public String getLocation(String ip) {
-        return "";
+        if (this.iptables == null || this.iptables.isEmpty()) {
+            return null;
+        }
+
+        String[] ips = ip.split(".");
+        if (ips.length >= 4) {
+            Long addr = (Long.parseLong(ips[0]) << 24) | (Long.parseLong(ips[1]) << 16 ) | (Long.parseLong(ips[2]) << 8) | Long.parseLong(ips[3]);
+
+            int start = 0;
+            int end = this.iptables.size();
+            while (end - start > 1) {
+                int mid = (start + end) / 2;
+                if (this.iptables.get(mid).getLeft() > addr) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }
+            }
+
+            return this.iptables.get(start).getRight();
+        }
+
+        return null;
     }
 
     public MediaBaseHandler getMediaHandler(int apiType) {
