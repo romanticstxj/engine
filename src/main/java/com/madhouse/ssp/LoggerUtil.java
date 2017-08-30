316d071e935acbd9fec174a4cbd2d68b12560d20
zhaoshengying <zhaoshengying@madhouse-inc.com>
2017-08-30 10:58:34 +0800

    update log
    Signed-off-by: zhaoshengying <zhaoshengying@madhouse-inc.com>

--- src/main/java/com/madhouse/ssp/LoggerUtil.java
+++ src/main/java/com/madhouse/ssp/LoggerUtil.java
@@ -1,9 +1,19 @@
 package com.madhouse.ssp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.Decoder;
+import org.apache.avro.io.DecoderFactory;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -42,7 +52,7 @@ public class LoggerUtil extends KafkaCallback {
 
     public void writeBidLog(KafkaProducer kafkaProducer, DSPBid.Builder message) {
         try {
-            sendMessage(kafkaProducer, message.build().toByteBuffer().array(),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_DSP_BID));
+            sendMessage(kafkaProducer, getAvroBytes(message.build()),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_DSP_BID));
         } catch (Exception e) {
             premiumMadLogger.error(e.toString());
         }
@@ -50,7 +60,7 @@ public class LoggerUtil extends KafkaCallback {
 
     public void writeMediaLog(KafkaProducer kafkaProducer, MediaBid.Builder message) {
         try {
-            sendMessage(kafkaProducer, message.build().toByteBuffer().array(),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_MEDIA_BID));
+            sendMessage(kafkaProducer, getAvroBytes(message.build()),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_MEDIA_BID));
         } catch (Exception e) {
             premiumMadLogger.error(e.toString());
         }
@@ -58,7 +68,7 @@ public class LoggerUtil extends KafkaCallback {
 
     public void writeWinNoticeLog(KafkaProducer kafkaProducer, WinNotice.Builder message) {
         try {
-            sendMessage(kafkaProducer, message.build().toByteBuffer().array(),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_WIN_NOTICE));
+            sendMessage(kafkaProducer, getAvroBytes(message.build()),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_WIN_NOTICE));
         } catch (Exception e) {
             premiumMadLogger.error(e.toString());
         }
@@ -66,7 +76,7 @@ public class LoggerUtil extends KafkaCallback {
 
     public void wirteImpressionTrackLog(KafkaProducer kafkaProducer, ImpressionTrack.Builder message) {
         try {
-            sendMessage(kafkaProducer, message.build().toByteBuffer().array(),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_IMPRESSION));
+            sendMessage(kafkaProducer, getAvroBytes(message.build()),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_IMPRESSION));
         } catch (Exception e) {
             premiumMadLogger.error(e.toString());
         }
@@ -74,7 +84,7 @@ public class LoggerUtil extends KafkaCallback {
 
     public void writeClickTrackLog(KafkaProducer kafkaProducer, ClickTrack.Builder message) {
         try {
-            sendMessage(kafkaProducer, message.build().toByteBuffer().array(),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_CLICK));
+            sendMessage(kafkaProducer, getAvroBytes(message.build()),ResourceManager.getInstance().getConfiguration().getKafka().getTopic(Constant.KafkaTopicType.KAFKA_CLICK));
         } catch (Exception e) {
             premiumMadLogger.error(e.toString());
         }
@@ -86,25 +96,31 @@ public class LoggerUtil extends KafkaCallback {
             if(null != message && null !=e){
                 String kafkaType = ResourceManager.getInstance().getConfiguration().getKafka().getkafka(message.topic);
                 String information = null;
+                Decoder decoder= DecoderFactory.get().binaryDecoder(message.message,null);
                 switch (kafkaType) {
                     case Constant.KafkaTopicType.KAFKA_CLICK:
-                        ClickTrack clickTrack = ClickTrack.fromByteBuffer(ByteBuffer.wrap(message.message));
+                        DatumReader<ClickTrack> clickTrackReader= new SpecificDatumReader<ClickTrack>(ClickTrack.class);
+                        ClickTrack clickTrack = clickTrackReader.read(null,decoder);
                         information = clickTrack.toString();
                         break;
                     case Constant.KafkaTopicType.KAFKA_DSP_BID:
-                        DSPBid dspBid = DSPBid.fromByteBuffer(ByteBuffer.wrap(message.message));
+                        DatumReader<DSPBid> dspBidReader = new SpecificDatumReader<DSPBid>(DSPBid.class);
+                        DSPBid dspBid = dspBidReader.read(null,decoder);
                         information = dspBid.toString();
                         break;
                     case Constant.KafkaTopicType.KAFKA_IMPRESSION:
-                        ImpressionTrack impressionTrack = ImpressionTrack.fromByteBuffer(ByteBuffer.wrap(message.message));
+                        DatumReader<ImpressionTrack> impressionTrackReader = new SpecificDatumReader<ImpressionTrack>(ImpressionTrack.class);
+                        ImpressionTrack impressionTrack = impressionTrackReader.read(null,decoder);
                         information = impressionTrack.toString();
                         break;
                     case Constant.KafkaTopicType.KAFKA_MEDIA_BID:
-                        MediaBid mediaBid = MediaBid.fromByteBuffer(ByteBuffer.wrap(message.message));
+                        DatumReader<MediaBid> mediaBidReader = new SpecificDatumReader<MediaBid>(MediaBid.class);
+                        MediaBid mediaBid = mediaBidReader.read(null,decoder);
                         information = mediaBid.toString();
                         break;
                     case Constant.KafkaTopicType.KAFKA_WIN_NOTICE:
-                        WinNotice winNotice = WinNotice.fromByteBuffer(ByteBuffer.wrap(message.message));
+                        DatumReader<WinNotice> readerwinNoticeReader = new SpecificDatumReader<WinNotice>(WinNotice.class);
+                        WinNotice winNotice = readerwinNoticeReader.read(null,decoder);
                         information = winNotice.toString();
                         break;
                 }
@@ -130,5 +146,14 @@ public class LoggerUtil extends KafkaCallback {
     
     private void sendMessage(KafkaProducer kafkaProducer,byte[] message,String topic ) {
         kafkaProducer.sendMessage(topic, message);
+    }
+    public byte[] getAvroBytes(SpecificRecordBase record) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        SpecificDatumWriter writer = new SpecificDatumWriter(record.getSchema());
+        Encoder encoder = EncoderFactory.get().binaryEncoder(out, null);
+        writer.write(record, encoder);
+        encoder.flush();
+        out.close();
+        return out.toByteArray();
     }
 }
