0ec1e294bd1aaba61e16340ba434eb7c04adf9ea
sunshao <97392368@qq.com>
2017-12-07 17:45:35 +0800

    oppo deals字段名错误，导致request json body转java对象缺失数据。

--- src/main/java/com/madhouse/media/oppo/OppoHandler.java
+++ src/main/java/com/madhouse/media/oppo/OppoHandler.java
@@ -146,10 +146,10 @@ public class OppoHandler extends MediaBaseHandler {
             }
         }
         if (ObjectUtils.isNotEmpty(imp.getPmp()) &&
-                ObjectUtils.isNotEmpty(imp.getPmp().getDelas()) &&
-                StringUtils.isNotEmpty(imp.getPmp().getDelas().get(0).getId())
+                ObjectUtils.isNotEmpty(imp.getPmp().getDeals()) &&
+                StringUtils.isNotEmpty(imp.getPmp().getDeals().get(0).getId())
                 ) {
-            List<OppoBidRequest.Imp.Pmp.Deal> delas = imp.getPmp().getDelas();
+            List<OppoBidRequest.Imp.Pmp.Deal> delas = imp.getPmp().getDeals();
             // 如果有多个pmp对象，随机取一个返回
             mediaRequest.setDealid(delas.get(Utility.nextInt(delas.size())).getId());
             imp.setImpressionType(OppoStatusCode.ImpressionType.PMP);
@@ -362,8 +362,8 @@ public class OppoHandler extends MediaBaseHandler {
                 bid.setAdm(JSON.toJSONString(oppoNativeResponse).toString());
 
             } else if (oppoBidRequest.getImp().get(0).getImpressionType() == OppoStatusCode.ImpressionType.PMP) {
-                if (null != oppoBidRequest.getImp().get(0).getPmp().getDelas() && oppoBidRequest.getImp().get(0).getPmp().getDelas().size() > 0) {
-                    bid.setDealid(oppoBidRequest.getImp().get(0).getPmp().getDelas().get(0).getId());
+                if (null != oppoBidRequest.getImp().get(0).getPmp().getDeals() && oppoBidRequest.getImp().get(0).getPmp().getDeals().size() > 0) {
+                    bid.setDealid(oppoBidRequest.getImp().get(0).getPmp().getDeals().get(0).getId());
                 }
             }
 
