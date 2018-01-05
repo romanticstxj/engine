d1ebb3a0953cfcd1ede3c15f5ef8ca451bba5789
zhaoshengying <zhaoshengying@madhouse-inc.com>
2018-01-05 14:04:27 +0800

    add yiche
    Signed-off-by: zhaoshengying <zhaoshengying@madhouse-inc.com>

--- src/main/java/com/madhouse/media/yiche/YiCheHandler.java
+++ src/main/java/com/madhouse/media/yiche/YiCheHandler.java
@@ -11,9 +11,15 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.madhouse.cache.CacheManager;
+import com.madhouse.cache.MaterialMetaData;
 import com.madhouse.cache.MediaBidMetaData;
 import com.madhouse.cache.MediaMappingMetaData;
+import com.madhouse.cache.MediaMetaData;
+import com.madhouse.cache.PlcmtMetaData;
 import com.madhouse.media.MediaBaseHandler;
+import com.madhouse.media.tencent.GPBForDSP.Request.Impression;
+import com.madhouse.media.yiche.YiCheBidRequest.Imp;
+import com.madhouse.resource.ResourceManager;
 import com.madhouse.ssp.Constant;
 import com.madhouse.ssp.avro.Geo;
 import com.madhouse.ssp.avro.MediaBid;
@@ -47,16 +53,16 @@ public class YiCheHandler extends MediaBaseHandler {
                     return false;
                 }
             	
-            	MediaRequest.Builder mediaRequest = this.conversionToPremiumMADData(bidRequest);
-            	if (mediaRequest == null) {
+            	List<MediaBid.Builder> mediaBids = this.conversionToPremiumMADData(bidRequest);
+            	if (ObjectUtils.isEmpty(mediaBids)) {
             		return false;
             	}
 
-            	if (!this.checkRequestParam(mediaRequest)) {
+            	if (!this.checkRequestParam(mediaBids.get(0).getRequestBuilder())) {
             		return false;
             	}
 
-            	mediaBidMetaData.getMediaBidBuilder().setRequestBuilder(mediaRequest);
+            	mediaBidMetaData.setMediaBids(mediaBids);
             	mediaBidMetaData.setRequestObject(bidRequest);
             	return true;
             } else {
@@ -69,7 +75,9 @@ public class YiCheHandler extends MediaBaseHandler {
 		return false;
 	}
 
-	private MediaRequest.Builder conversionToPremiumMADData(YiCheBidRequest bidRequest) {
+	private List<MediaBid.Builder> conversionToPremiumMADData(YiCheBidRequest bidRequest) {
+		List<MediaBid.Builder> mediaBids = new LinkedList<>();
+		
 		MediaRequest.Builder mediaRequest = MediaRequest.newBuilder();
 
 		try {
@@ -78,39 +86,10 @@ public class YiCheHandler extends MediaBaseHandler {
                 return null;
             }
 			
-			YiCheBidRequest.Imp imp = bidRequest.getImp().get(0);
 			YiCheBidRequest.Device device = bidRequest.getDevice();
 			YiCheBidRequest.User user = bidRequest.getUser();
 			
-			if (imp.getPmp() != null) {
-                if (!ObjectUtils.isEmpty(imp.getPmp().getDeals())) {
-                    int size = imp.getPmp().getDeals().size();
-                    mediaRequest.setDealid(StringUtil.toString(imp.getPmp().getDeals().get(Utility.nextInt(size)).getId()));
-                }
-            }
-			StringBuilder adspaceKey = new StringBuilder();
-            adspaceKey.append("YICHE:").append(StringUtil.toString(imp.getTagid()));;
-			
-			
-            if(YiCheConstant.OsType.ANDROID == device.getOs()){
-            	adspaceKey.append(":ANDROID");
-                mediaRequest.setOs(Constant.OSType.ANDROID);
-                mediaRequest.setDidmd5(StringUtil.toString(device.getDidmd5()));
-                mediaRequest.setDid(user.getId());
-            } else if (YiCheConstant.OsType.IOS == device.getOs()) {
-                adspaceKey.append(":IOS");
-                mediaRequest.setOs(Constant.OSType.IOS);
-                mediaRequest.setDpidmd5(user.getId().toLowerCase());
-            }
-            MediaMappingMetaData mediaMappingMetaData = CacheManager.getInstance().getMediaMapping(adspaceKey.toString());
-            if (mediaMappingMetaData == null) {
-                return null;
-            }
-            
-            
             mediaRequest.setAdtype(2);
-            mediaRequest.setAdspacekey(mediaMappingMetaData.getAdspaceKey());
-            mediaRequest.setBid(StringUtil.toString(bidRequest.getId()));
             mediaRequest.setIp(StringUtil.toString(device.getIp()));
             String ua = StringEscapeUtils.escapeJava(device.getUa());
             mediaRequest.setUa(StringUtils.isEmpty(ua) ? "YICHE" : ua);
@@ -118,9 +97,8 @@ public class YiCheHandler extends MediaBaseHandler {
             mediaRequest.setOsv(StringUtil.toString(StringUtils.isEmpty(device.getOsv()) ? "1.0.0" : device.getOsv() ));
             mediaRequest.setMacmd5(StringUtil.toString(device.getMacmd5()));
             mediaRequest.setTest(Constant.Test.REAL);
-            
-            
             mediaRequest.setCarrier(Constant.Carrier.UNKNOWN);
+            
             mediaRequest.setConnectiontype(Constant.ConnectionType.UNKNOWN);
             if (device.getConnectiontype() != null) {
                 switch (device.getConnectiontype()) {
@@ -174,18 +152,53 @@ public class YiCheHandler extends MediaBaseHandler {
             builder.setLat(0);
             builder.setLon(0);
             mediaRequest.setGeoBuilder(builder);
-            
-            if (imp.getBanner() != null) {
-                YiCheBidRequest.Imp.Banner banner = imp.getBanner();
-                if(banner.getW() != null){
-                	mediaRequest.setW(banner.getW());
-                }
-                if(banner.getH() != null){
-                	mediaRequest.setH(banner.getH());
-                }
-            }
 			
-            return mediaRequest;
+            for (Imp imp : bidRequest.getImp()) {
+                MediaBid.Builder mediaBid = MediaBid.newBuilder();
+                MediaRequest.Builder request = MediaRequest.newBuilder(mediaRequest);
+                
+                if (imp.getPmp() != null) {
+                    if (!ObjectUtils.isEmpty(imp.getPmp().getDeals())) {
+                        int size = imp.getPmp().getDeals().size();
+                        request.setDealid(StringUtil.toString(imp.getPmp().getDeals().get(Utility.nextInt(size)).getId()));
+                    }
+                }
+    			StringBuilder adspaceKey = new StringBuilder();
+                adspaceKey.append("YICHE:").append(StringUtil.toString(imp.getTagid()));;
+                
+                if(YiCheConstant.OsType.ANDROID == device.getOs()){
+                	adspaceKey.append(":ANDROID");
+                	request.setOs(Constant.OSType.ANDROID);
+                	request.setDidmd5(StringUtil.toString(device.getDidmd5()));
+                	request.setDid(user.getId());
+                } else if (YiCheConstant.OsType.IOS == device.getOs()) {
+                    adspaceKey.append(":IOS");
+                    request.setOs(Constant.OSType.IOS);
+                    request.setDpidmd5(user.getId().toLowerCase());
+                }
+                MediaMappingMetaData mediaMappingMetaData = CacheManager.getInstance().getMediaMapping(adspaceKey.toString());
+                if (mediaMappingMetaData == null) {
+                    break;
+                }
+                
+                request.setBid(StringUtil.toString(imp.getId()));
+                request.setAdspacekey(mediaMappingMetaData.getAdspaceKey());
+                
+                
+                if (imp.getBanner() != null) {
+                    YiCheBidRequest.Imp.Banner banner = imp.getBanner();
+                    if(banner.getW() != null){
+                    	request.setW(banner.getW());
+                    }
+                    if(banner.getH() != null){
+                    	request.setH(banner.getH());
+                    }
+                }
+                mediaBid.setRequestBuilder(request);
+                mediaBids.add(mediaBid);
+            }
+            
+            return mediaBids;
         } catch (Exception e) {
             logger.error("yiche Exception:{}" ,e.toString());
         }
@@ -197,47 +210,49 @@ public class YiCheHandler extends MediaBaseHandler {
 	public boolean packageMediaResponse(MediaBidMetaData mediaBidMetaData,
 			HttpServletResponse resp) {
 		try {
-			 if (mediaBidMetaData != null && mediaBidMetaData.getMediaBidBuilder() != null && mediaBidMetaData.getMaterialMetaData() != null) {
-				 if (mediaBidMetaData.getMediaBidBuilder().getStatus() == Constant.StatusCode.OK) {
-					 MediaBid.Builder mediaBid = mediaBidMetaData.getMediaBidBuilder();
-					 MediaResponse.Builder mediaResponse = mediaBid.getResponseBuilder();
-					 YiCheBidRequest bidRequest = (YiCheBidRequest)mediaBidMetaData.getRequestObject();
-					 
-					 YiCheBidResponse bidResponse = new YiCheBidResponse();
-					 bidResponse.setId(bidRequest.getId());
-					 bidResponse.setBidid(mediaBid.getImpid());
-					 
-					 YiCheBidResponse.SeatBid.Bid bid = new YiCheBidResponse.SeatBid.Bid();
-					 
-					 bid.setId(mediaBid.getImpid());
-					 bid.setImpid(StringUtil.toString(bidRequest.getImp().get(0).getId()));
-					 bid.setAdid(StringUtil.toString(mediaBidMetaData.getMaterialMetaData().getMediaQueryKey()));
-					 bid.setDealid(StringUtil.toString(mediaBid.getRequestBuilder().getDealid()));
-					 
-					 Monitor.Builder monitor = mediaResponse.getMonitorBuilder();
-					 if (monitor != null) {
-						 if (ObjectUtils.isNotEmpty(monitor.getImpurl())) {
-							 bid.setDurl(new LinkedList<>());
-							 List<Track> listTrack = monitor.getImpurl();
-							 bid.getDurl().add(monitor.getImpurl().get(0).getUrl());
-							 bid.getDurl().add(monitor.getImpurl().get(listTrack.size()-1).getUrl());
+			 if (mediaBidMetaData != null && ObjectUtils.isNotEmpty(mediaBidMetaData.getMediaBids())) {
+				 YiCheBidRequest bidRequest = (YiCheBidRequest)mediaBidMetaData.getRequestObject();
+				 YiCheBidResponse bidResponse = new YiCheBidResponse();
+				 bidResponse.setId(bidRequest.getId());
+				 bidResponse.setBidid(ResourceManager.getInstance().nextId());
+				 
+				 YiCheBidResponse.SeatBid seatBid = new YiCheBidResponse.SeatBid();
+				 bidResponse.setSeatbid(new LinkedList<>());
+				 
+				 seatBid.setBid(new LinkedList<>());
+				 
+				 for (MediaBid.Builder mediaBid : mediaBidMetaData.getMediaBids()) {
+					 if (mediaBid.getStatus() == Constant.StatusCode.OK) {
+						 MediaBidMetaData.BidMetaData bidMetaData = mediaBidMetaData.getBidMetaDataMap().get(mediaBid.getImpid());
+						 MaterialMetaData materialMetaData = bidMetaData.getMaterialMetaData();
+						 MediaResponse.Builder mediaResponse = mediaBid.getResponseBuilder();
+						 
+						 YiCheBidResponse.SeatBid.Bid bid = new YiCheBidResponse.SeatBid.Bid();
+						 bid.setId(mediaBid.getImpid());
+						 bid.setImpid(StringUtil.toString(mediaBid.getRequestBuilder().getBid()));
+						 bid.setAdid(StringUtil.toString(materialMetaData.getMediaQueryKey()));
+						 bid.setDealid(StringUtil.toString(mediaBid.getRequestBuilder().getDealid()));
+						 
+						 Monitor.Builder monitor = mediaResponse.getMonitorBuilder();
+						 if (monitor != null) {
+							 if (ObjectUtils.isNotEmpty(monitor.getImpurl())) {
+								 bid.setDurl(new LinkedList<>());
+								 List<Track> listTrack = monitor.getImpurl();
+								 bid.getDurl().add(monitor.getImpurl().get(0).getUrl());
+								 bid.getDurl().add(monitor.getImpurl().get(listTrack.size()-1).getUrl());
+							 }
+							 if (ObjectUtils.isNotEmpty(monitor.getClkurl())) {
+								 List<String> clkUrls = monitor.getClkurl();
+								 bid.setCurl(StringUtil.toString(clkUrls.get(clkUrls.size()-1) + "&_url=" +mediaResponse.getLpgurl()));
+							 }
 						 }
-						 if (ObjectUtils.isNotEmpty(monitor.getClkurl())) {
-							 List<String> clkUrls = monitor.getClkurl();
-							 bid.setCurl(StringUtil.toString(clkUrls.get(clkUrls.size()-1) + "&_url=" +mediaResponse.getLpgurl()));
-						 }
+						 seatBid.getBid().add(bid);
+						 
 					 }
-					 
-					 YiCheBidResponse.SeatBid seatBid = new YiCheBidResponse.SeatBid();
-					 bidResponse.setSeatbid(new LinkedList<>());
-					 
-					 seatBid.setBid(new LinkedList<>());
-					 seatBid.getBid().add(bid);
-					 
-					 bidResponse.getSeatbid().add(seatBid);
-					 return outputStreamWrite(resp, bidResponse);
-					 
 				 }
+				 bidResponse.getSeatbid().add(seatBid);
+				 return outputStreamWrite(resp, bidResponse);
+				 
 			 }
 		} catch (Exception e) {
 			logger.error("yiche Exception:{}" ,e.toString());
